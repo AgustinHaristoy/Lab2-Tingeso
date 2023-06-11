f35f834e1f53811e4ba572a56552a55d6083cc6a
Agustin Haristoy <agustin.haristoy@usach.cl>
2023-06-10 21:24:00 -0400

    Proveedor-service

---
+++ proveedor-service/src/main/java/com/usach/proveedorservice/ProveedorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.usach.proveedorservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProveedorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProveedorServiceApplication.class, args);
+	}
+
+}
